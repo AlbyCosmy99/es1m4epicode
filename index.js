616efdfa2ff457ddb054b114a4ebe104e47330c5
Andrei <albu.cosminandrei.1999@gmail.com>
2023-10-23 00:21:19 +0200

    changes

--- index.js
+++ index.js
@@ -19,3 +19,37 @@ function firstLetters(str) {
 
     return newStr
 }
+
+function countMaxChar(str) {
+    if(!str) return -1
+
+    strArr = str.split('')
+    let dict = {}
+    strArr.forEach(char => {
+        if(!dict[char]) {
+            dict[char] = 1
+        }
+        else {
+            dict[char] = dict[char] + 1
+        }
+    })
+
+    let max = strArr[0]
+    for(let i=1;i<strArr.length;i++) {
+        if(dict[max] < dict[strArr[i]]){
+            max = strArr[i]
+        }
+    }
+    return max
+}
+
+function printHash(n) {
+    let str = ''
+    for(let i=1;i<=n;i++) {
+        for(let j=1;j<=n;j++) {
+            if(j <= i) str+='#'
+        }
+        str +='\n'
+    }
+    if(str!=='') console.log(str)
+}
